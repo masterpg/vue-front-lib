56529ec57c2ddb6f9da589507436af427601161e
masterpg <masterpg@livedeveloper.net>
2018-04-01 19:16:47 +0900

    webpack.config.jsを開発と本番を想定して一旦分割。今回は開発用だけ。

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -2,22 +2,6 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  mode: 'development',
-
-  // ビルドの起点となるファイルパスを設定。
-  // このファイルからrequireされているファイルが芋づる式に取得されることになる。
-  entry: {
-    'index': './src/index.ts'
-  },
-
-  output: {
-    // Webpackに生成したファイルの格納場所を設定
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js',
-    // HMRに必要な設定①
-    // 参照: http://dackdive.hateblo.jp/entry/2016/05/07/183335
-    publicPath: '/dist'
-  },
 
   resolve: {
     // この拡張子のファイルをビルド対象に設定
@@ -33,7 +17,7 @@ module.exports = {
       {
         enforce: 'pre',
         test: /\.ts?$/,
-        loader: 'tslint-loader'
+        loader: 'tslint-loader',
       },
       {
         test: /\.ts$/,
@@ -43,7 +27,7 @@ module.exports = {
           // 「.vue」のファイルに接尾辞「.ts」がファイル名に追加されるよう設定
           // 参照: https://github.com/TypeStrong/ts-loader#user-content-appendtssuffixto-regexp-default
           appendTsSuffixTo: [/\.vue$/]
-        }
+        },
       },
       // 「.vue」ファイルをvue-loaderがハンドルするよう設定
       // 参照: https://github.com/vuejs-templates/webpack-simple/blob/master/template/webpack.config.js
@@ -55,47 +39,25 @@ module.exports = {
           scss: 'vue-style-loader!css-loader!sass-loader',
           loaders: {
             ts: 'ts-loader!tslint-loader'
-          }
-        }
+          },
+        },
       },
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader'
+        loader: 'style-loader!css-loader',
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
-        loader: 'file-loader'
+        loader: 'file-loader',
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
         loader: 'file-loader',
         query: {
-          name: '[name].[ext]?[hash]'
-        }
-      }
-    ]
+          name: '[name].[ext]?[hash]',
+        },
+      },
+    ],
   },
-
-  devtool: 'source-map',
-
-  // webpack-dev-serverの設定
-  devServer: {
-    contentBase: '.',
-    port: 5000,
-    host: '0.0.0.0',
-    disableHostCheck: true,
-    // historyApiFallbackの設定は以下URLを参照:
-    // https://github.com/webpack/docs/wiki/webpack-dev-server#the-historyapifallback-option
-    historyApiFallback: {
-      rewrites: [],
-    },
-    // HMRに必要な設定②
-    hot: true,
-  },
-
-  plugins: [
-    // HMRに必要な設定③
-    new webpack.HotModuleReplacementPlugin(),
-  ]
 
 };
---
+++ webpack.config.dev.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.config.base.js');
+
+module.exports = merge(baseConfig, {
+
+  mode: 'development',
+
+  entry: {
+    'index': './src/index.ts',
+  },
+
+  output: {
+    // Webpackに生成したファイルの格納場所を設定
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+    // HMRに必要な設定①
+    // 参照: http://dackdive.hateblo.jp/entry/2016/05/07/183335
+    publicPath: '/dist'
+  },
+
+  devtool: 'source-map',
+
+  // webpack-dev-serverの設定
+  devServer: {
+    contentBase: '.',
+    port: 5000,
+    host: '0.0.0.0',
+    disableHostCheck: true,
+    // historyApiFallbackの設定は以下URLを参照:
+    // https://github.com/webpack/docs/wiki/webpack-dev-server#the-historyapifallback-option
+    historyApiFallback: {
+      rewrites: [],
+    },
+    // HMRに必要な設定②
+    hot: true,
+  },
+
+  plugins: [
+    // HMRに必要な設定③
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+
+});
