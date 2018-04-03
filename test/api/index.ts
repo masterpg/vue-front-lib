688d61d0d5f20a459ff4be50d34dc928cd2ccf8a
masterpg <masterpg@livedeveloper.net>
2018-04-03 22:41:15 +0900

    単体テストの環境を構築

---
+++ test/api/index.ts
@@ -0,0 +1,10 @@
+debugger;
+const assert = chai.assert;
+
+suite('api', () => {
+
+  test('サンプルテスト', () => {
+    assert.equal(1, 1);
+  });
+
+});
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,11 +3,13 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.config.base.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 /**
  * ビルド結果の出力パス
  */
-const OUTPUT_PATH = '.dist';
+const OUTPUT_PATH = 'dist';
 
 /**
  * 基準パス
@@ -20,6 +22,7 @@ module.exports = merge(baseConfig, {
 
   entry: {
     'index': './src/index.ts',
+    'test': './test/test.ts',
   },
 
   output: {
@@ -50,16 +53,37 @@ module.exports = merge(baseConfig, {
   },
 
   plugins: [
+    new CleanWebpackPlugin(
+      [OUTPUT_PATH],
+      { verbose: true },
+    ),
+
+    new webpack.IgnorePlugin(/vertx/),
+
     // HMRに必要な設定③
     new webpack.HotModuleReplacementPlugin(),
 
     new HtmlWebpackPlugin({
       filename: 'index.html', // パスは`output.path`を基準
-      template: 'src/index.html',
+      template: './src/index.html',
       inject: false,
       basePath: BASE_PATH,
       bundledScript: '<script type="text/javascript" src="index.bundle.js"></script>',
     }),
+
+    new HtmlWebpackPlugin({
+      filename: 'test.html', // パスは`output.path`を基準
+      template: './test/test.html',
+      inject: false,
+      bundledScript: '<script type="text/javascript" src="test.bundle.js"></script>',
+    }),
+
+    // `to: xxx`の`xxx`は`output.path`が基準になる
+    new CopyWebpackPlugin([
+      { from: 'node_modules/mocha/mocha.css', to: 'node_modules/mocha' },
+      { from: 'node_modules/mocha/mocha.js', to: 'node_modules/mocha' },
+      { from: 'node_modules/chai/chai.js', to: 'node_modules/chai' },
+    ]),
   ],
 
 });
