57ee5be098fb09b952211d222ce4e0ea1f113678
masterpg <masterpg@livedeveloper.net>
2018-06-03 12:36:51 +0900

    API基盤を一部実装

--- src/app/apis/base.ts
+++ src/app/apis/base.ts
@@ -1,5 +1,68 @@
 import Vue from 'vue';
+import axios from 'axios';
+
+export interface ApiRequestConfig {
+  headers?: any;
+  params?: any;
+  paramsSerializer?: (params: any) => string;
+  responseType?: string;
+}
+
+export interface ApiResponse<T = any> {
+  data: T;
+  status: number;
+  statusText: string;
+  headers: any;
+  config: ApiRequestConfig;
+  request?: any;
+}
+
+export interface ApiError extends Error {
+  config: ApiRequestConfig;
+  code?: string;
+  request?: any;
+  response?: ApiResponse;
+}
+
+export interface ApiPromise<T = any> extends Promise<ApiResponse<T>> {
+}
+
+interface ApiRequestInternalConfig extends ApiRequestConfig {
+  url: string;
+  method: string;
+  data?: any;
+}
 
 export abstract class BaseApi extends Vue {
+  get<T = any>(path: string, config?: ApiRequestConfig): ApiPromise<T> {
+    return this.request(Object.assign(config || {}, {
+      url: path, method: 'get',
+    }));
+  }
 
+  delete(path: string, config?: ApiRequestConfig): ApiPromise {
+    return this.request(Object.assign(config || {}, {
+      url: path, method: 'delete',
+    }));
+  }
+
+  post<T = any>(path: string, data?: any, config?: ApiRequestConfig): ApiPromise<T> {
+    return this.request(Object.assign(config || {}, {
+      url: path, method: 'post', data,
+    }));
+  }
+
+  put<T = any>(path: string, data?: any, config?: ApiRequestConfig): ApiPromise<T> {
+    return this.request(Object.assign(config || {}, {
+      url: path, method: 'put', data,
+    }));
+  }
+
+  request<T = any>(config: ApiRequestInternalConfig): ApiPromise<T> {
+    const apiInfo = this.$config.apiInfo;
+    const axiosConfig = Object.assign(config || {}, {
+      baseURL: `${apiInfo.protocol}://${apiInfo.host}:${apiInfo.port}/api/`,
+    });
+    return axios.request(axiosConfig);
+  }
 }
--- src/app/apis/shop-api/index.ts
+++ src/app/apis/shop-api/index.ts
@@ -2,21 +2,11 @@ import Component from 'vue-class-component';
 import { BaseApi } from '../base';
 import { Product, ShopApi } from '../types';
 
-import axios from 'axios';
-
-const SAMPLE_DATA: Product[] = [
-  { id: 1, title: 'iPad 4 Mini', price: 500.01, inventory: 2 },
-  { id: 2, title: 'H&M T-Shirt White', price: 10.99, inventory: 10 },
-  { id: 3, title: 'Charli XCX - Sucker CD', price: 19.99, inventory: 5 },
-];
-
 @Component
 class ShopApiImpl extends BaseApi implements ShopApi {
   async getProducts(): Promise<Product[]> {
-    const apiInfo = this.$config.apiInfo;
-    const url = `${apiInfo.protocol}://${apiInfo.host}:${apiInfo.port}/api/products`;
-    const response = await axios.get(url, {});
-    return response.data as Product[];
+    const response = await this.get<Product[]>('products');
+    return response.data;
   }
 
   buyProducts(products: Array<{ id: number, quantity: number }>): Promise<void> {
