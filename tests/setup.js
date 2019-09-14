51f8c85788f9c992dbbc0407eb7f43630ef4f741
masterpg <masterpg@livedeveloper.net>
2019-09-14 13:42:22 +0900

    Firebaseのjsファイルをindex.htmlで読み込むよう変更した際、単テストでエラーが出るようになってしまったので修正

--- tests/setup.js
+++ tests/setup.js
@@ -1,6 +1,7 @@
 const td = require('testdouble')
 require('testdouble-jest')(td, jest)
 
+global.firebase = require('firebase')
 const utils = require('@/base/utils')
 const config = require('@/base/config')
 
