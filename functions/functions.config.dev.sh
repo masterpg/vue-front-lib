bb6caf91aa93d121ea93b969323b23527012b022
masterpg <masterpg@livedeveloper.net>
2019-10-11 16:41:26 +0900

    ユーザーロール設定部分の修正で以前のコミット漏れがあったので対応

--- functions/functions.config.dev.sh
+++ functions/functions.config.dev.sh
@@ -4,4 +4,4 @@ firebase functions:config:set \
   functions.region="asia-northeast1" \
   storage.bucket="gs://vue-base-project-7295.appspot.com/" \
   cors.whitelist="http://localhost, http://localhost:5000, http://localhost:5010, chrome-extension://aejoelaoggembcahagimdiliamlcdmfm" \
-  role.admins="taro@example.com"
+  role.app.admins="taro@example.com"
--- functions/functions.config.prod.sh
+++ functions/functions.config.prod.sh
@@ -4,4 +4,4 @@ firebase functions:config:set \
   functions.region="asia-northeast1" \
   storage.bucket="gs://vue-base-project-7295.appspot.com/" \
   cors.whitelist="https://vue-base-project-7295.web.app, https://vue-base-project-7295.firebaseapp.com" \
-  role.admins="taro@exaple.com"
+  role.app.admins="taro@exaple.com"
