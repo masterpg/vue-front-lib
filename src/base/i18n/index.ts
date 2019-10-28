12b16dcb623932c150f989d41ce10e97d9fb7826
masterpg <masterpg@livedeveloper.net>
2019-10-28 23:34:24 +0900

    i18nのロジックを修正

--- src/base/i18n/index.ts
+++ src/base/i18n/index.ts
@@ -5,16 +5,34 @@ import { dateTimeFormats } from '@/base/i18n/date-time-formats'
 
 Vue.use(VueI18n)
 
-interface LocaleData {
+export class LocaleData {
+  constructor(locale: VueI18n.Locale)
+
+  constructor(language: string, country: string)
+
+  constructor(localeOrLanguage: VueI18n.Locale | string, country?: string) {
+    if (country) {
+      this.language = localeOrLanguage.toLowerCase()
+      this.country = country.toUpperCase()
+    } else {
+      const array = localeOrLanguage.split('-')
+      this.language = array[0].toLowerCase()
+      this.country = array[1].toUpperCase()
+    }
+    this.locale = `${this.language}-${this.country}`
+  }
+
   language: string
+
   country: string
+
   locale: VueI18n.Locale
 }
 
 /**
  * 本プロジェクト用にVueI18nを拡張したクラスです。
  */
-export class AppI18n extends VueI18n {
+export abstract class BaseI18n extends VueI18n {
   //----------------------------------------------------------------------
   //
   //  Constructors
@@ -46,6 +64,40 @@ export class AppI18n extends VueI18n {
   //
   //----------------------------------------------------------------------
 
+  /**
+   * アプリケーションがサポートする言語+国の配列です。
+   * 国はその言語のデフォルトとなる国を指定します。
+   * 例: ['ja-JP', 'en-US', 'de-DE']
+   */
+  protected abstract readonly supportLocales: LocaleData[]
+
+  /**
+   * デフォルトのロケール(言語+国)です。
+   * 例: 'ja-JP'
+   */
+  protected abstract readonly defaultLocale: LocaleData
+
+  /**
+   * 言語リソースを読み込みます。
+   */
+  async load(): Promise<void> {
+    // ロケールデータを取得(ブラウザから)
+    const localeData = this.m_getLocaleDataFromBrowser()
+    const newLanguage = localeData.language
+
+    // 現在の言語とロケールデータの言語が異なる場合、ロケールデータの言語をロード
+    const currentLanguage = this.m_localeData ? this.m_localeData.language : ''
+    if (currentLanguage !== newLanguage) {
+      if (this.m_loadedLanguages.indexOf(newLanguage) === -1) {
+        const msgs = await import(/* webpackChunkName: "lang-[request]" */ `@/lang/${newLanguage}`)
+        this.setLocaleMessage(newLanguage, msgs.default)
+      }
+    }
+
+    // ロケールデータの設定
+    this.m_setLocaleData(localeData)
+  }
+
   d(value: number | Date, key?: VueI18n.Path, locale?: VueI18n.Locale): VueI18n.DateTimeFormatResult
   d(value: number | Date, args?: { [key: string]: string }): VueI18n.DateTimeFormatResult
   d(arg1: number | Date, arg2?: any, arg3?: any): VueI18n.DateTimeFormatResult {
@@ -53,7 +105,7 @@ export class AppI18n extends VueI18n {
     // 引数にロケールが指定された場合
     if (arg3 && typeof arg3 === 'string') {
       // 指定されたロケールからロケールデータを作成
-      const localeData = LocaleUtil.createLocaleData(arg3)
+      const localeData = this.m_toLocaleData(arg3)
       // "en-US"や"ja-JP"などを設定
       locale = localeData.locale
     }
@@ -65,22 +117,6 @@ export class AppI18n extends VueI18n {
     return super.d(arg1, arg2, locale)
   }
 
-  /**
-   * 言語リソースを読み込みます。
-   */
-  async load(): Promise<void> {
-    const localeData = this.m_getLocaleData()
-    const currentLanguage = this.m_localeData ? this.m_localeData.language : ''
-    const newLanguage = localeData.language
-    if (currentLanguage !== newLanguage) {
-      if (this.m_loadedLanguages.indexOf(newLanguage) === -1) {
-        const msgs = await import(/* webpackChunkName: "lang-[request]" */ `@/lang/${newLanguage}`)
-        this.setLocaleMessage(newLanguage, msgs.default)
-      }
-    }
-    this.m_setLanguage(localeData)
-  }
-
   //----------------------------------------------------------------------
   //
   //  Internal methods
@@ -90,7 +126,7 @@ export class AppI18n extends VueI18n {
   /**
    * ロケールデータを取得します。
    */
-  private m_getLocaleData(): LocaleData {
+  private m_getLocaleDataFromBrowser(): LocaleData {
     // ブラウザから言語+国を取得("en"や"en-US"などを取得)
     const locale =
       (window.navigator.languages && window.navigator.languages[0]) ||
@@ -98,78 +134,100 @@ export class AppI18n extends VueI18n {
       (window.navigator as any).userLanguage ||
       (window.navigator as any).browserLanguage
     // 取得した言語+国からロケールデータを作成
-    return LocaleUtil.createLocaleData(locale)
+    return this.m_toLocaleData(locale)
   }
 
   /**
    * ロケールデータの設定を行います。
    * @param localeData
    */
-  private m_setLanguage(localeData: LocaleData): void {
+  private m_setLocaleData(localeData: LocaleData): void {
     this.locale = localeData.language
     axios.defaults.headers.common['Accept-Language'] = localeData.language
     document.querySelector('html')!.setAttribute('lang', localeData.language)
 
     this.m_localeData = localeData
+
     this.m_loadedLanguages = this.m_loadedLanguages || []
     if (this.m_loadedLanguages.indexOf(localeData.language) === -1) {
-      this.m_loadedLanguages = [localeData.language]
+      this.m_loadedLanguages.push(localeData.language)
     }
+  }
+
+  /**
+   * ロケール文字列をロケールデータ(`LocaleData`)に変換します。
+   * @param locale
+   */
+  private m_toLocaleData(locale: VueI18n.Locale): LocaleData {
+    // 言語+国("en-US"など)を分割
+    const { language, country } = this.m_splitLocale(locale)
+
+    let result: LocaleData
+
+    // 言語がサポートされている場合
+    if (this.m_isLanguageSupported(language)) {
+      if (country) {
+        result = new LocaleData(language, country)
+      } else {
+        result = new LocaleData(language, this.m_getDefaultLanguageCountry(language)!)
+      }
+    }
+    // 言語がサポートされていない場合
+    else {
+      result = this.defaultLocale
+    }
+
+    return result
+  }
+
+  /**
+   * ロケール(言語+国)を言語と国に分割します。
+   * @param locale
+   */
+  private m_splitLocale(locale: string): { language: string; country?: string } {
+    const array = locale.split('-')
+    const language = array[0].toLowerCase()
+    const country = array.length >= 2 ? array[1].toUpperCase() : undefined
+    return { language, country }
+  }
+
+  /**
+   * 指定された言語がサポートされるかを取得します。
+   * @param language
+   */
+  private m_isLanguageSupported(language: string): boolean {
+    for (const localeData of this.supportLocales) {
+      if (localeData.language.toLowerCase() === language.toLowerCase()) {
+        return true
+      }
+    }
+    return false
+  }
+
+  /**
+   * 指定された言語に対するデフォルトの国を取得します。
+   * @param language
+   */
+  private m_getDefaultLanguageCountry(language: string): string | undefined {
+    for (const localeData of this.supportLocales) {
+      if (localeData.language === language) {
+        return localeData.country
+      }
+    }
+    return undefined
   }
 }
 
 /**
- * ロケールを操作するためのユーティリティクラスです。
+ * アプリケーション用のi18nです。
  */
-class LocaleUtil {
-  /**
-   * ロケールデータを作成します。
-   * @param locale
-   */
-  static createLocaleData(locale: VueI18n.Locale): LocaleData {
-    // 言語+国("en-US")を分割
-    const localeArray = locale.split('-')
-    let language: string
-    let country: string
-    // 言語+国が空だった場合
-    if (localeArray.length === 0) {
-      // デフォルトの言語と国を取得
-      language = 'en'
-      country = LocaleUtil.m_getDefaultCountry(language)
-    }
-    // 言語+国が設定されていた場合
-    else {
-      // 言語を取得
-      language = localeArray[0]
-      // 国を取得(国が指定されていなかった場合はデフォルトの国が取得される)
-      if (localeArray.length === 2) {
-        country = LocaleUtil.m_getDefaultCountry(language, localeArray[1])
-      } else {
-        country = LocaleUtil.m_getDefaultCountry(language)
-      }
-    }
-    return { language, country, locale: `${language}-${country}` }
+export class AppI18n extends BaseI18n {
+  protected get supportLocales(): LocaleData[] {
+    return [new LocaleData('ja-JP'), new LocaleData('en-US')]
   }
 
-  /**
-   * 指定された言語と国からデフォルトの国を取得します。
-   * @param language
-   * @param country
-   */
-  private static m_getDefaultCountry(language: string, country?: string): string {
-    // 国が指定されている場合はその国を返す
-    if (country) {
-      return country.toUpperCase()
-    }
-    // 国が指定されていなかった場合、言語をもとにデフォルトの国を返す
-    switch (language) {
-      case 'en':
-        return 'US'
-      case 'ja':
-        return 'JP'
-      default:
-        return 'US'
-    }
+  protected get defaultLocale(): LocaleData {
+    return this.supportLocales[0]
   }
 }
 
@@ -178,10 +236,10 @@ class LocaleUtil {
  * node_modules/vue-i18n/dist/vue-i18n.esm.js
  *
  * 改変理由:
- * comp-tree-viewはテンプレートに記述するのではなくプログラムでnewしてインスタンス化
- * している。プログラムでnewした場合、$i18nのようにインジェクションされないライブラリ
- * が存在する。この対応として以下ではインジェクションされていない場合、アプリケーション
- * がインスタンス化したi18nを使用するよう改変している。
+ * comp-tree-viewはコンポーネントのインスタンス化をテンプレートに記述するのではなく
+ * プログラムでnewしている。プログラムでnewした場合、$i18nのようにインジェクション
+ * されないライブラリが存在する。この対応として$i18nがインジェクションされていない場合、
+ * 以下のようにアプリケーションがインスタンス化したi18nを使用するよう改変している。
  */
 Vue.prototype.$t = function(key) {
   const values: any[] = []
